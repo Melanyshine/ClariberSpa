8f7763363949f22a1ab0ccb8fc266248c713d8fb
natylvs <nataliagomezcoca16@gmail.com>
2026-05-21 07:54:33 -0400

    Hice el diseño de Historial Citas, agregue herramientas y codigo

--- Capa Presentacion/FrmHistorialCitas.Designer.cs
+++ Capa Presentacion/FrmHistorialCitas.Designer.cs
@@ -30,41 +30,54 @@
         {
             this.dgvHistorial = new System.Windows.Forms.DataGridView();
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.panelTabla = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).BeginInit();
+            this.panelTabla.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvHistorial
             // 
             this.dgvHistorial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHistorial.Location = new System.Drawing.Point(73, 69);
+            this.dgvHistorial.Location = new System.Drawing.Point(127, 118);
             this.dgvHistorial.Name = "dgvHistorial";
             this.dgvHistorial.RowHeadersWidth = 62;
             this.dgvHistorial.RowTemplate.Height = 28;
-            this.dgvHistorial.Size = new System.Drawing.Size(643, 314);
+            this.dgvHistorial.Size = new System.Drawing.Size(729, 409);
             this.dgvHistorial.TabIndex = 0;
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Location = new System.Drawing.Point(297, 23);
+            this.lblTitulo.Location = new System.Drawing.Point(309, 31);
             this.lblTitulo.Name = "lblTitulo";
             this.lblTitulo.Size = new System.Drawing.Size(128, 20);
             this.lblTitulo.TabIndex = 1;
             this.lblTitulo.Text = "Historial de Citas";
             // 
+            // panelTabla
+            // 
+            this.panelTabla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTabla.Controls.Add(this.dgvHistorial);
+            this.panelTabla.Controls.Add(this.lblTitulo);
+            this.panelTabla.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTabla.Location = new System.Drawing.Point(0, 0);
+            this.panelTabla.Name = "panelTabla";
+            this.panelTabla.Size = new System.Drawing.Size(1017, 612);
+            this.panelTabla.TabIndex = 2;
+            // 
             // FrmHistorialCitas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblTitulo);
-            this.Controls.Add(this.dgvHistorial);
+            this.ClientSize = new System.Drawing.Size(1017, 612);
+            this.Controls.Add(this.panelTabla);
             this.Name = "FrmHistorialCitas";
             this.Text = "FrmHistorialCitas";
             this.Load += new System.EventHandler(this.FrmHistorialCitas_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvHistorial)).EndInit();
+            this.panelTabla.ResumeLayout(false);
+            this.panelTabla.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -72,5 +85,6 @@
 
         private System.Windows.Forms.DataGridView dgvHistorial;
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel panelTabla;
     }
 }
--- Capa Presentacion/FrmHistorialCitas.cs
+++ Capa Presentacion/FrmHistorialCitas.cs
@@ -1,40 +1,136 @@
 ﻿using CapaNegocio;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace CapaPresentacion
 {
     public partial class FrmHistorialCitas : Form
     {
-        Historial_CitaBLL bll = new Historial_CitaBLL();
+        Historial_CitaBLL bll =
+            new Historial_CitaBLL();
+
+        // 🎨 COLORES
+        Color colorRosado =
+            Color.RosyBrown;
+
+        Color fondo =
+            Color.FromArgb(250, 248, 246);
 
         public FrmHistorialCitas()
         {
             InitializeComponent();
         }
 
-        private void FrmHistorialCitas_Load(object sender, EventArgs e)
+        private void FrmHistorialCitas_Load(
+            object sender,
+            EventArgs e)
         {
+            AplicarDiseno();
+
             try
             {
-                dgvHistorial.DataSource = bll.Listar();
+                dgvHistorial.DataSource =
+                    bll.Listar();
+
+                // 🔥 OCULTAR IDS
+                if (dgvHistorial.Columns.Contains("id_historial"))
+                    dgvHistorial.Columns["id_historial"].Visible = false;
+
+                if (dgvHistorial.Columns.Contains("id_cita"))
+                    dgvHistorial.Columns["id_cita"].Visible = false;
             }
             catch (Exception ex)
             {
                 MessageBox.Show(
-                    "Error al cargar historial:\n" + ex.Message,
+                    "Error al cargar historial:\n" +
+                    ex.Message,
                     "Error",
                     MessageBoxButtons.OK,
                     MessageBoxIcon.Error);
             }
-
         }
 
-        private void BtnHistorial_Click(object sender, EventArgs e)
+        private void BtnHistorial_Click(
+            object sender,
+            EventArgs e)
         {
-            FrmHistorialCitas frm = new FrmHistorialCitas();
+            FrmHistorialCitas frm =
+                new FrmHistorialCitas();
+
             frm.Show();
         }
 
+        // 🎨 DISEÑO
+        private void AplicarDiseno()
+        {
+            // FORM
+            this.BackColor =
+                fondo;
+
+            // TITULO
+            lblTitulo.ForeColor =
+                colorRosado;
+
+            lblTitulo.Font =
+                new Font(
+                    "Segoe UI Semibold",
+                    22F,
+                    FontStyle.Bold);
+
+            // PANEL TABLA
+            panelTabla.BackColor =
+                Color.White;
+
+            // TABLA
+            dgvHistorial.BackgroundColor =
+                Color.White;
+
+            dgvHistorial.BorderStyle =
+                BorderStyle.None;
+
+            dgvHistorial.RowHeadersVisible =
+                false;
+
+            dgvHistorial.AutoSizeColumnsMode =
+                DataGridViewAutoSizeColumnsMode.Fill;
+
+            dgvHistorial.EnableHeadersVisualStyles =
+                false;
+
+            dgvHistorial.ColumnHeadersBorderStyle =
+                DataGridViewHeaderBorderStyle.None;
+
+            dgvHistorial.ColumnHeadersDefaultCellStyle.BackColor =
+                colorRosado;
+
+            dgvHistorial.ColumnHeadersDefaultCellStyle.ForeColor =
+                Color.White;
+
+            dgvHistorial.ColumnHeadersDefaultCellStyle.Font =
+                new Font(
+                    "Segoe UI Semibold",
+                    10F);
+
+            dgvHistorial.ColumnHeadersHeight =
+                38;
+
+            dgvHistorial.DefaultCellStyle.Font =
+                new Font(
+                    "Segoe UI",
+                    10F);
+
+            dgvHistorial.DefaultCellStyle.SelectionBackColor =
+                Color.FromArgb(230, 210, 215);
+
+            dgvHistorial.DefaultCellStyle.SelectionForeColor =
+                Color.Black;
+
+            dgvHistorial.AlternatingRowsDefaultCellStyle.BackColor =
+                Color.FromArgb(248, 244, 242);
+
+            dgvHistorial.GridColor =
+                Color.FromArgb(235, 230, 228);
+        }
     }
 }
